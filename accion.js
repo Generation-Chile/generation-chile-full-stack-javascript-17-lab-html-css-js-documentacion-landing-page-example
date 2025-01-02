6a2bf71b0838ce4cb787888d74e71029cff1f728
Romina Rivera <romina.rivera@genstudents.org>
2025-01-02 17:11:25 -0300

    año es variable

--- accion.js
+++ accion.js
@@ -1,31 +1,30 @@
+function presionarBoton() {
+  let nombre = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let mensaje = document.getElementById('message').value;
+  if (nombre === '' || email === '' || mensaje === '') {
+    alert('Por favor, complete todos los campos');
+    return;
+  }
+  alert('Gracias por enviar tu mensaje, revisa la consola por favor');
+  console.log({ nombre, email, mensaje });
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('message').value = '';
+}
 
+function getCurrentYear() {
+  const today = new Date();
+
+  return today.getFullYear();
+}
 
 let form = document.getElementById('cta-form');
-
-form.addEventListener('click', function() {
+form.addEventListener('click', function () {
   let formElement = document.getElementById('form');
-formElement.scrollIntoView({ behavior: 'smooth' });
+  formElement.scrollIntoView({ behavior: 'smooth' });
 });
 
-  function presionarBoton() {
-    // capturar los valores de los inputs
-    let nombre = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let mensaje = document.getElementById('message').value;
-    // enviar los valores en un objeto a la consola
-    
+const paragraphFooter = document.getElementById('year');
 
-    if(nombre === '' || email === '' || mensaje === '') {
-      alert('Por favor, complete todos los campos');
-      return;
-    }
-
-    // mostrar un mensaje de alerta
-    alert('Gracias por enviar tu mensaje, revisa la consola por favor');
-    console.log({nombre, email, mensaje});
-    // limpiar los inputs
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-  }
-  
+paragraphFooter.textContent = `\u00A9 ${getCurrentYear()} FullStack Developers. Todos los derechos reservados`;
